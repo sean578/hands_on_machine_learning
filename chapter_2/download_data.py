8dc697f4a8d281a7d8e395e22d5d725886ab8cdd
Sean <sean@cortirio.com>
2019-11-02 13:33:07 +0000

    Download and extract housing dataset

---
+++ chapter_2/download_data.py
@@ -0,0 +1,23 @@
+import os
+import tarfile
+import urllib.request
+
+DOWNLOAD_ROOT = "https://raw.githubusercontent.com/ageron/handson-ml2/master/"
+HOUSING_PATH = os.path.join("datasets", "housing")
+HOUSING_URL = DOWNLOAD_ROOT + "datasets/housing/housing.tgz"
+
+
+def fetch_housing_data(housing_url=HOUSING_URL, housing_path=HOUSING_PATH):
+    if not os.path.isdir(housing_path):
+        os.makedirs(housing_path)
+    tgz_path = os.path.join(housing_path, "housing.tgz")
+    urllib.request.urlretrieve(housing_url, tgz_path)
+    housing_tgz = tarfile.open(tgz_path)
+    housing_tgz.extractall(path=housing_path)
+    housing_tgz.close()
+
+
+if __name__ == '__main__':
+
+    fetch_housing_data()
+    print('Downloaded data')
